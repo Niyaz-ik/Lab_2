c9c43614c22fc9eea6eaff4cc24558d0e610bbdb
Niyazik <fexfulfexful@gmail.com>
2020-02-18 04:36:03 +0300

    Third commit

--- src/GenericItem.java
+++ src/GenericItem.java
@@ -1,14 +1,14 @@
 /**
  * GenericItem class describes a single item from an online store.
- * */
+ */
 
-public class GenericItem {
+public class GenericItem implements Cloneable{
     /**
      * @param ID - product ID
      * @param name - product name
      * @param price - price of the product
      * @param type - type of product
-     * */
+     */
     public int ID;
     public String name;
     public float price;
@@ -16,7 +16,7 @@ public class GenericItem {
 
     public Category current = Category.GENERAL;
 
-    String GetCategory(Category strmain) {
+    public String GetCategory(Category strmain) {
         String str = "";
         current = strmain;
         switch (current) {
@@ -37,9 +37,79 @@ public class GenericItem {
     }
 
     /**
-     * This method will print all fields of an instance of the GenericItem class:*/
-    void printAll(Category strmain) {
-        GetCategory(strmain);
-        System.out.printf("ID: %d, Name: %s, price: %5.2f, type: %s , current: %s\n", ID, name, price, type, strmain);
+     * This method will print all fields of an instance of the GenericItem class:
+     */
+    void printAll() {
+        GetCategory(current);
+        System.out.printf("ID: %d, Name: %s, price: %5.2f, type: %s, current: %s\n", ID, name, price, type, current);
+    }
+
+    public boolean equals (TechnicalItem obj1){
+        TechnicalItem obj2 = (TechnicalItem) obj1;
+
+        if ((this.ID != obj2.ID) || (this.name != obj2.name)
+                || (this.price != obj2.price) || (this.type != obj2.type) || (this.current != obj2.current))
+            return false;
+
+        return true;
+    }
+
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
+
+    public String toString() {
+        return getClass().getName() + "@" + this.ID + this.name + this.price + this.type + this.current;
+    }
+}
+
+class FoodItem extends GenericItem {
+    String dateOfIncome; // дата производства
+    short expires; //срок годности (суток)
+
+    void printAll() {
+        GetCategory(current);
+        System.out.printf("ID: %d, Name: %s, price: %5.2f, type: %s, current: %s, data: %s, expires (day): %d\n",
+                ID, name, price, type, current, dateOfIncome, expires);
+    }
+
+    public boolean equals (FoodItem obj1){
+        FoodItem obj2 = (FoodItem) obj1;
+
+        if ((this.ID != obj2.ID) || (this.name != obj2.name)
+                || (this.price != obj2.price) || (this.type != obj2.type) || (this.current != obj2.current)
+                || (this.dateOfIncome != obj2.dateOfIncome) || (this.expires != obj2.expires))
+            return false;
+
+        return true;
+    }
+
+    public String toString() {
+        return getClass().getName() + "@" + this.ID + this.name + this.price + this.type + this.current + this.dateOfIncome + this.expires;
+    }
+}
+
+class TechnicalItem extends GenericItem {
+    short warrantyTime; // гарантийный срок (суток)
+
+    void printAll() {
+        GetCategory(current);
+        System.out.printf("ID: %d, Name: %s, price: %5.2f, type: %s, current: %s, warranty time (day): %d\n",
+                ID, name, price, type, current, warrantyTime);
+    }
+
+    public boolean equals (TechnicalItem obj1){
+        TechnicalItem obj2 = (TechnicalItem) obj1;
+
+        if ((this.ID != obj2.ID) || (this.name != obj2.name)
+                || (this.price != obj2.price) || (this.type != obj2.type) || (this.current != obj2.current)
+                || (this.warrantyTime != obj2.warrantyTime))
+            return false;
+
+        return true;
+    }
+
+    public String toString() {
+        return getClass().getName() + "@" + this.ID + this.name + this.price + this.type + this.current + this.warrantyTime;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,131 @@
 
 public class Main {
     public static void main (String[] args){
-        GenericItem Kor = new GenericItem();
-        Kor.ID = 100;
-        Kor.name = "Korovka";
-        Kor.price = 67;
-        Kor.type = "Milk";
-        Kor.current = Category.FOOD;
-        Kor.printAll(Kor.current);
+        GenericItem Jacket = new GenericItem();
+        Jacket.ID = 120;
+        Jacket.name = "Jacket-MWC";
+        Jacket.price = 3000;
+        Jacket.type = "windproof";
+        Jacket.current = Category.DRESS;
+        Jacket.printAll();
+        System.out.println("");
+
+        PrintMassive();
+
+        System.out.println("");
+        FoodItem Milk1 = new FoodItem();
+        Milk1.ID = 200;
+        Milk1.name = "Korovka";
+        Milk1.price = 67;
+        Milk1.type = "Milk";
+        Milk1.current = Category.FOOD;
+        Milk1.dateOfIncome = "10.10.2019";
+        Milk1.expires = 7;
+
+        FoodItem Milk2 = new FoodItem();
+        Milk2.ID = 201;
+        Milk2.name = "Prostokvashino";
+        Milk2.price = 70;
+        Milk2.type = "Milk";
+        Milk2.current = Category.FOOD;
+        Milk2.dateOfIncome = "10.10.2019";
+        Milk2.expires = 7;
+
+
+        CheckEqualObjects(Milk1, Milk2); //Сравнивает два молочных объекта
+
+        FoodItem Milk4 = GetClone(Milk1);
+        CheckEqualObjects(Milk1, Milk4);
+
+        Milk1.printAll();
+        Milk4.printAll();
+
+        System.out.println(Milk1.toString());
+        System.out.println(Jacket.toString());
+
+    }
+
+    public static GenericItem GetClone(GenericItem Obj2){
+        GenericItem Obj1 = null;
+        try {
+            Obj1 = (GenericItem) Obj2.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return Obj1;
+    }
+
+    public static FoodItem GetClone(FoodItem Obj2){
+        FoodItem Obj1 = null;
+        try {
+            Obj1 = (FoodItem) Obj2.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return Obj1;
+    }
+
+    public static TechnicalItem GetClone(TechnicalItem Obj2){
+        TechnicalItem Obj1 = null;
+        try {
+            Obj1 = (TechnicalItem) Obj2.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return Obj1;
+    }
+
+    public static void PrintMassive(){
+        GenericItem Product[] = new GenericItem[10];
+        for (int i = 0; i < 5; i++) {
+            FoodItem a = new FoodItem();
+            a.ID = 100 + i;
+            a.name = "Korovka";
+            a.price = 67;
+            a.type = "Milk";
+            a.current = Category.FOOD;
+            a.dateOfIncome = "10.10.2019";
+            a.expires = 7;
+            Product[i] = a;
+        }
+
+        for (int i = 5; i < 10; i++) {
+            TechnicalItem a = new TechnicalItem();
+            a.ID = 100 + i;
+            a.name = "Iphone 11 Pro";
+            a.price = 99000;
+            a.type = "Apple";
+            a.current = Category.GENERAL;
+            a.warrantyTime = 365;
+            Product[i] = a;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Product[i].printAll();
+        }
+    }
+
+    public static void CheckEqualObjects(GenericItem Obj1, GenericItem Obj2){
+        if (Obj1.equals(Obj2)){
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are equal");
+        } else {
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are not equal");
+        }
+    }
+
+    public static void CheckEqualObjects(FoodItem Obj1, FoodItem Obj2){
+        if (Obj1.equals(Obj2)){
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are equal");
+        } else {
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are not equal");
+        }
+    }
+
+    public static void CheckEqualObjects(TechnicalItem Obj1, TechnicalItem Obj2){
+        if (Obj1.equals(Obj2)){
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are equal");
+        } else {
+            System.out.println("Products " + Obj1.name+ " and " + Obj2.name + " are not equal");
+        }
     }
 }
